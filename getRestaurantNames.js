4c14fc7200c6c498c98ad9c148472358efdf21f2
Rick Zhang <rzhang@paperlesspost.com>
2019-05-17 00:25:01 -0400

    getRestaurantNames content script added

---
+++ getRestaurantNames.js
@@ -0,0 +1,3 @@
+const selectors = '.u-text-ellipsis';
+const restaurantNames = document.querySelectorAll('.u-text-ellipsis');
+restaurantNames.forEach(function (node) { console.log(node.innerText) });
