53382796d3cf17c9c16ad61c9f94cb849406d1df
Rick <rickzhang@live.ca>
2019-05-18 11:05:47 -0400

    Renaming variable to be more expressive

--- getRestaurantNames.js
+++ getRestaurantNames.js
@@ -1,13 +1,13 @@
 window.addEventListener('load', getRestaurantNames, false);
 
 async function getRestaurantNames() {
-  const selector = '.u-text-ellipsis';
-  while(!document.querySelector(selector)) {
+  const restaurantNameSelector = '.u-text-ellipsis';
+  while(!document.querySelector(restaurantNameSelector)) {
     await new Promise(r => setTimeout(r, 1000));
   }
 
   let names = [];
-  const nodes = document.querySelectorAll(selector).forEach((node) => names.push(node.innerText));
+  const nodes = document.querySelectorAll(restaurantNameSelector).forEach((node) => names.push(node.innerText));
   
   chrome.runtime.sendMessage({names: names}, function(response) {
     console.log(response);
