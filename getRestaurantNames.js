9240cbb51305aacc3e345db2f962cf8423917ed1
Rick Zhang <rickzhang@live.ca>
2019-05-17 22:01:33 -0400

    Waiting for the DOM element of interest

--- getRestaurantNames.js
+++ getRestaurantNames.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', getRestaurantNames, false);
 
-function getRestaurantNames() {
+async function getRestaurantNames() {
   const selector = '.u-text-ellipsis';
+  while(!document.querySelector(selector)) {
+    await new Promise(r => setTimeout(r, 1000));
+  }
+
   const restaurantNames = document.querySelectorAll(selector);
   restaurantNames.forEach(function (node) { console.log(node.innerText) });
 }
