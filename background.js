301fd66e72e8d120e97f35c8537758afc8be0a02
Rick <rickzhang@live.ca>
2019-05-18 11:06:55 -0400

    Allow for requests to be made to the Places API

--- background.js
+++ background.js
@@ -1,8 +1,48 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-      "from a content script:" + sender.tab.url :
-      "from the extension");
-    request.names ? sendResponse({ received: "true"}) : sendResponse({ received: "false"});
+    const url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=HIDDEN&inputtype=textquery&locationbias=ipbias&fields=rating,user_ratings_total&input=' + encodeURIComponent(request.names[0]);
+
+    makeRequest(url)
+      .then(resp => {
+        const parsedJSON = JSON.parse(resp);
+        if (parsedJSON.candidates.length) {
+          sendResponse({
+            name: request.names[0],
+            rating: parsedJSON.candidates[0].rating,
+            userRatings: parsedJSON.candidates[0].user_ratings_total,
+          });
+        } else {
+          sendResponse({
+            error: 'no candidates',
+          })
+        }
+       })
+      .catch(err => { sendResponse({ err }); })
+
+      return true;
   }
 );
+
+function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function(){
+      if (xhr.status >= 200 && xhr.status < 300){
+          return resolve(xhr.response);
+      } else {
+        reject(Error({
+          status: xhr.status,
+          statusTextInElse: xhr.statusText
+        }))
+      }
+    }
+    xhr.onerror = function(){
+      reject(Error({
+        status: xhr.status,
+        statusText: xhr.statusText
+      }))
+    }
+    xhr.send();
+  })
+}
