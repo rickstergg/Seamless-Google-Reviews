e1ebc45825d443e050859c358647cdb8b5cec960
Rick <rickzhang@live.ca>
2019-05-18 17:22:41 -0400

    Reference CSS, replace seamless node with google node

--- getRestaurantNames.js
+++ getRestaurantNames.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', getRestaurantNames, false);
 
+function createGoogleRating(percentage) {
+  let node = document.createElement('div');
+  node.className = 'google-ratings';
+
+  node.innerHTML = '<div class="stars-container"><div class="google-ratings-top" style="width: ' + percentage + '%"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div><div class="google-ratings-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div></div>';
+
+  return node;
+}
+
 async function getRestaurantNames() {
   const restaurantNameSelector = '.u-text-ellipsis';
   while(!document.querySelector(restaurantNameSelector)) {
@@ -10,6 +19,13 @@ async function getRestaurantNames() {
   const nodes = document.querySelectorAll(restaurantNameSelector).forEach((node) => names.push(node.innerText));
   
   chrome.runtime.sendMessage({names: names}, function(response) {
-    console.log(response);
+    const {
+      name,
+      rating,
+      userRatingCount,
+    } = response;
+
+    const seamlessRatingNode = document.querySelector('.restaurantCard-rating');
+    seamlessRatingNode.parentNode.replaceChild(createGoogleRating(rating * 20), seamlessRatingNode);
   });
 }
