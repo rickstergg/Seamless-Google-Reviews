9c96dbad480b20e850e7529f1ccd648559d0484c
Rick Zhang <rickzhang@live.ca>
2022-05-29 21:19:39 -0400

    Update the seamless project to support manifest v3

--- background.js
+++ background.js
@@ -1,23 +1,39 @@
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+makeRequest = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.log('error', err))
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message === 'getRestaurantNames') {
-      const key = 'HIDDEN';
+      const googleApiKey = '';
       let googleRequests = [];
       request.names.forEach((name) => {
-        const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${key}&inputtype=textquery&locationbias=ipbias&fields=name,rating,user_ratings_total&input=${encodeURIComponent(name)}`;
+        const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${googleApiKey}&inputtype=textquery&locationbias=ipbias&fields=name,rating,user_ratings_total&input=${encodeURIComponent(name)}`;
         googleRequests.push(makeRequest(url));
       });
 
       Promise.all(googleRequests).then((apiResponses) => {
         const massagedData = apiResponses.map(data => {
-          const parsedData = JSON.parse(data);
-          if (parsedData.candidates.length) {
-            return {
-              rating: parsedData.candidates[0].rating,
-              userRatingCount: parsedData.candidates[0].user_ratings_total
-            };
+          if (data) {
+            if (data.candidates.length) {
+              return {
+                rating: data.candidates[0].rating,
+                userRatingCount: data.candidates[0].user_ratings_total
+              };
+            } else {
+              return { error: 'no data' };
+            }
           } else {
-            return { error: 'no data' };
+            return { error: 'parsing undefined' }
           }
         });
 
@@ -32,38 +48,17 @@ chrome.runtime.onMessage.addListener(
 );
 
 chrome.webRequest.onCompleted.addListener(
-  function(details) {
-    chrome.tabs.executeScript({
-      file: 'content.js'
-    });
+  async function(details) {
+    const tab = await getCurrentTab();
+    if (tab) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content.js']
+      });
+    }
   },
   {
     urls: ["*://*.seamless.com/search*", "*://api-gtm.grubhub.com/restaurants/search*"],
     types: ['xmlhttprequest']
   }
 );
-
-
-makeRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-          return resolve(xhr.response);
-      } else {
-        reject(Error({
-          status: xhr.status,
-          statusTextInElse: xhr.statusText
-        }))
-      }
-    }
-    xhr.onerror = () => {
-      reject(Error({
-        status: xhr.status,
-        statusText: xhr.statusText
-      }))
-    }
-    xhr.send();
-  });
-}
